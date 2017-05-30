2d6546dcfa95070499e461a5fa1ddf980a1ac661
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-05-30 16:18:08 -0400

    list of months

---
+++ src/NameOfMonths.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NameOfMonths {
+	
+	static int noOfDays = 30;
+	
+	public static void main(String[] args){
+		
+		System.out.print("Please enter the no of Days");
+		Scanner scn = new Scanner(System.in);
+		noOfDays = scn.nextInt();
+		printMonths();
+	}
+	
+	public static void printMonths(){
+		
+		if (noOfDays == 30 || noOfDays == 30 || noOfDays == 30){
+			
+			switch (noOfDays){
+		
+				case 30 : System.out.print("April  June  September  November");
+							break;
+				case 31 : System.out.print("January  March  May  July  August  October  December");
+							break; 
+				case 28: System.out.print("February");
+		         			break;
+			}
+		
+	
+		} else {
+			System.out.println("There is no month with only " + noOfDays + " days");
+		}
+	}
+}
